4924758001b3ca57f3e80988c6b22172cede77a3
recep <recepoezmen1@gmail.com>
2026-04-28 11:50:47 +0200

    add:05 pom update und 2c klasse

---
+++ Labor_VerteilteSysteme/src/main/java/aufgabe2c/SystemProducer.java
@@ -0,0 +1,42 @@
+package aufgabe2c;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class SystemProducer {
+
+    public static void main(String[] args) throws Exception {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "10.50.15.52:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        ObjectMapper mapper = new ObjectMapper();
+        OperatingSystemMXBean os = ManagementFactory.getOperatingSystemMXBean();
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        System.out.println("Producer gestartet...");
+
+        while (true) {
+            Map<String, Object> data = new HashMap<>();
+            data.put("cpu", os.getSystemLoadAverage());
+            data.put("cores", os.getAvailableProcessors());
+            data.put("time", System.currentTimeMillis());
+
+            String json = mapper.writeValueAsString(data);
+
+            producer.send(new ProducerRecord<>("vlvs_inf22_gruppeB", "system", json));
+            System.out.println("Gesendet: " + json);
+
+            Thread.sleep(5000);
+        }
+    }
+}
